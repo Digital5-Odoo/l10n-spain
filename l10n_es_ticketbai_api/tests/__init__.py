14331109cff54d7f8168b9903dee8f55e1c630e6
Enrique Martín <enriquemartin@digital5.es>
2024-07-29 16:11:52 +0200

    [IMP] l10n_es_ticketbai* - usamos modelo aeat.tax.agency

--- l10n_es_ticketbai_api/tests/__init__.py
+++ l10n_es_ticketbai_api/tests/__init__.py
@@ -1,4 +1,3 @@
 from . import common
 from . import test_l10n_es_ticketbai_customer_invoice
 from . import test_l10n_es_ticketbai_customer_cancellation
-from . import test_l10n_es_ticketbai_tax_agency
