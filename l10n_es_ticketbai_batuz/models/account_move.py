288ab36bd027cb1f523ff16472eed86b64fd3292
Enrique Martín <enriquemartin@digital5.es>
2024-08-22 14:14:01 +0200

    [IMP] l10n_es_ticketbai* - eliminamos relaciones duplicadas para diferenciar altas y anulaciones

--- l10n_es_ticketbai_batuz/models/account_move.py
+++ l10n_es_ticketbai_batuz/models/account_move.py
@@ -100,14 +100,11 @@ class AccountMove(models.Model):
     @api.depends(
         "tbai_invoice_ids",
         "tbai_invoice_ids.state",
-        "tbai_cancellation_ids",
-        "tbai_cancellation_ids.state",
     )
     def _compute_lroe_response_line_ids(self):
+        response_line_model = self.env["lroe.operation.response.line"]
         for record in self:
-            response_line_model = self.env["lroe.operation.response.line"]
             response_ids = record.tbai_invoice_ids.mapped("tbai_response_ids").ids
-            response_ids += record.tbai_cancellation_ids.mapped("tbai_response_ids").ids
             response_lines = response_line_model.search(
                 [("tbai_response_id", "in", response_ids)]
             )
