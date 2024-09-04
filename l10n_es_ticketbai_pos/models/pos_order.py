8615addd1bfbb69fcc905f3f1f6f9b7e6bf831bf
Enrique Martín <enriquemartin@digital5.es>
2024-09-04 11:45:43 +0200

    [ADD] l10n_es_ticketbai_pos - soporte para ticketbai en TPV (solo online) Rebase 16/07/25: debido a rebase tenemos conflictos, descartamos todos los cambios entrantes y nos quedamos con los cambios propios y borramos los archivos no necesarios

--- l10n_es_ticketbai_pos/models/pos_order.py
+++ l10n_es_ticketbai_pos/models/pos_order.py
@@ -1,128 +1,166 @@
 # Copyright 2021 Binovo IT Human Project SL
 # Copyright 2022 Landoo Sistemas de Informacion SL
+# Copyright 2024 Digital5, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-import base64
 
-from odoo import Command, _, api, exceptions, fields, models
+from odoo import _, api, exceptions, fields, models
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
-
-from odoo.addons.l10n_es_ticketbai_api.models.ticketbai_invoice import (
-    SiNoType,
-    TicketBaiInvoiceState,
-)
-from odoo.addons.l10n_es_ticketbai_api.models.ticketbai_invoice_tax import (
-    NotExemptedType,
-)
-from odoo.addons.l10n_es_ticketbai_api.ticketbai.xml_schema import TicketBaiSchema
 
 
 class PosOrder(models.Model):
-    _inherit = "pos.order"
+    _name = "pos.order"
+    _inherit = ["pos.order", "tbai.mixin"]
 
-    tbai_enabled = fields.Boolean(related="company_id.tbai_enabled", readonly=True)
-    tbai_invoice_id = fields.Many2one(
-        comodel_name="tbai.invoice", string="TicketBAI PoS Order", copy=False
+    tbai_identifier = fields.Char(
+        string="TBAI Identifier",
+        related="tbai_invoice_id.tbai_identifier",
+        store=True,
+        copy=False,
+    )
+    tbai_qr_src = fields.Char(
+        string="QR source", compute="_compute_tbai_qr_src", store=True, copy=False
     )
     tbai_invoice_ids = fields.One2many(
         comodel_name="tbai.invoice",
         inverse_name="pos_order_id",
         string="TicketBAI Invoices",
     )
-    tbai_response_ids = fields.Many2many(
-        comodel_name="tbai.response",
-        compute="_compute_tbai_response_ids",
-        string="Responses",
-    )
-    tbai_vat_regime_key = fields.Many2one(
-        comodel_name="tbai.vat.regime.key", string="VAT Regime Key"
-    )
 
     @api.depends("tbai_invoice_ids", "tbai_invoice_ids.state")
     def _compute_tbai_response_ids(self):
         for record in self:
             record.tbai_response_ids = [
-                Command.set(record.tbai_invoice_ids.mapped("tbai_response_ids").ids)
+                (6, 0, record.tbai_invoice_ids.mapped("tbai_response_ids").ids)
             ]
+
+    @api.depends("date_order")
+    def _compute_tbai_datetime_invoice(self):
+        for record in self:
+            record.tbai_datetime_invoice = fields.Datetime.now()
+
+    @api.depends("tbai_invoice_id", "tbai_invoice_id.qr")
+    def _compute_tbai_qr_src(self):
+        for po in self:
+            tbai_qr_src = ""
+            if po.tbai_invoice_id:
+                tbai_qr_src = "data:image/png;base64," + str(
+                    po.tbai_invoice_id.qr.decode("UTF-8")
+                )
+            po.tbai_qr_src = tbai_qr_src
 
     @api.model
     def _order_fields(self, ui_order):
         res = super()._order_fields(ui_order)
-        if ui_order.get("tbai_vat_regime_key", False) and not ui_order.get(
-            "to_invoice", False
-        ):
-            res["tbai_vat_regime_key"] = ui_order.get("tbai_vat_regime_key")
+        session = self.env["pos.session"].browse(ui_order["pos_session_id"])
+        if session.config_id.tbai_enabled and not ui_order.get("to_invoice", False):
+            res["tbai_vat_regime_key"] = ui_order.get("tbai_vat_regime_key", False)
         return res
 
-    def tbai_prepare_invoice_values(self, pos_order=None):
-        self.ensure_one()
-        partner = self.partner_id
-        prefix = self.tbai_get_value_serie_factura()
-        number = self.tbai_get_value_num_factura()
-        expedition_date = self.tbai_get_value_fecha_expedicion_factura()
-        expedition_hour = self.tbai_get_value_hora_expedicion_factura()
-        vals = {
-            "schema": TicketBaiSchema.TicketBai.value,
-            "company_id": self.company_id.id,
-            "simplified_invoice": SiNoType.S.value,
-            "pos_order_id": self.id,
-            "name": self.l10n_es_unique_id,
-            "number_prefix": prefix,
-            "number": number,
-            "expedition_date": expedition_date,
-            "expedition_hour": expedition_hour,
-            "description": "/",
-            "amount_total": f"{self.amount_total:.2f}",  # noqa: E231
-            "vat_regime_key": self.tbai_vat_regime_key.code,
-            "state": TicketBaiInvoiceState.pending.value,
-        }
-        if partner:
-            vals["tbai_customer_ids"] = [
-                Command.create(
-                    {
-                        "name": partner.tbai_get_value_apellidos_nombre_razon_social(),
-                        "country_code": partner._parse_aeat_vat_info()[0],
-                        "nif": partner.tbai_get_value_nif(),
-                        "identification_number": partner.tbai_partner_identification_number
-                        or partner.vat,
-                        "idtype": partner.tbai_partner_idtype,
-                        "address": partner.tbai_get_value_direccion(),
-                        "zip": partner.zip,
-                    }
-                )
-            ]
-        if pos_order is None:
-            vals["previous_tbai_invoice_id"] = self.config_id.tbai_last_invoice_id.id
-        else:
-            previous_order_pos_reference = pos_order.get(
-                "tbai_previous_order_pos_reference", False
+    @api.model
+    def _process_order(self, pos_order, draft, existing_order):
+        if draft:
+            return super()._process_order(pos_order, draft, existing_order)
+        if pos_order["data"].get("tbai_vat_regime_key", False) and isinstance(
+            pos_order["data"]["tbai_vat_regime_key"], str
+        ):
+            regime_key = pos_order["data"]["tbai_vat_regime_key"]
+            pos_order["data"]["tbai_vat_regime_key"] = (
+                self.env["tbai.vat.regime.key"]
+                .search([("code", "=", regime_key)], limit=1)
+                .id
             )
-            if previous_order_pos_reference:
-                tbai_previous_order = self.search(
-                    [("l10n_es_unique_id", "=", previous_order_pos_reference)], limit=1
+        order_id = super()._process_order(pos_order, draft, existing_order)
+        order = self.env["pos.order"].browse(order_id)
+        if order.config_id.tbai_enabled and not pos_order["data"].get(
+            "to_invoice", False
+        ):
+            order._tbai_build_invoice()
+            order.config_id.tbai_last_invoice_id = order.tbai_invoice_id
+        return order.id
+
+    def _export_for_ui(self, order):
+        res = super()._export_for_ui(order)
+        res["tbai_identifier"] = order.tbai_identifier
+        res["tbai_qr_src"] = order.tbai_qr_src
+        return res
+
+    def _prepare_invoice_vals(self):
+        res = super(PosOrder, self)._prepare_invoice_vals()
+        if self.tbai_enabled:
+            vat_regime_key_id = self.tbai_vat_regime_key
+            if not vat_regime_key_id and self.partner_id:
+                fp = self.env["account.fiscal.position"].get_fiscal_position(
+                    self.partner_id.id
                 )
-                vals[
-                    "previous_tbai_invoice_id"
-                ] = tbai_previous_order.tbai_invoice_id.id
-            datas = base64.b64encode(pos_order["data"]["tbai_datas"].encode("utf-8"))
-            vals.update(
-                {
-                    "datas": datas,
-                    "datas_fname": "{}.xsig".format(
-                        self.l10n_es_unique_id.replace("/", "-")
-                    ),
-                    "file_size": len(datas),
-                    "signature_value": pos_order["data"]["tbai_signature_value"],
-                }
-            )
-        gipuzkoa_tax_agency = self.env.ref(
-            "l10n_es_ticketbai_api.tbai_tax_agency_gipuzkoa"
+                vat_regime_key_id = fp.tbai_vat_regime_key.id
+            if not vat_regime_key_id:
+                vat_regime_key_id = self.env.ref(
+                    "l10n_es_ticketbai.tbai_vat_regime_01"
+                ).id
+            res.update({"tbai_vat_regime_key": vat_regime_key_id})
+        return res
+
+    def tbai_prepare_invoice_values(self):
+        vals = super().tbai_prepare_invoice_values()
+        vals.update(
+            {
+                "name": self.l10n_es_unique_id,
+                "pos_order_id": self.id,
+                "amount_total": "%.2f" % self.amount_total,
+                "previous_tbai_invoice_id": self.config_id.tbai_last_invoice_id.id,
+                "state": "pending",
+            }
         )
-        araba_tax_agency = self.env.ref("l10n_es_ticketbai_api.tbai_tax_agency_araba")
-        tax_agency = self.company_id.tbai_tax_agency_id
-        is_gipuzkoa_tax_agency = tax_agency == gipuzkoa_tax_agency
-        is_araba_tax_agency = tax_agency == araba_tax_agency
-        taxes = {}
+        return vals
+
+    def _tbai_build_invoice(self):
+        tbai_inv_obj = self.env["tbai.invoice"]
+        for record in self:
+            vals = record.tbai_prepare_invoice_values()
+            tbai_invoice = tbai_inv_obj.create(vals)
+            tbai_invoice.sudo().build_tbai_simplified_invoice()
+            record.tbai_invoice_id = tbai_invoice.id
+
+    def tbai_prepare_cancellation_values(self):
+        vals = super().tbai_prepare_cancellation_values()
+        vals.update(
+            {
+                "pos_order_id": self.id,
+            }
+        )
+        return vals
+
+    def tbai_prepare_invoice_lines_values(self):
+        self.ensure_one()
         lines = []
+        for line in self.lines:
+            description_line = line.name[:250] if line.name else ""
+            if (
+                self.company_id.tbai_protected_data
+                and self.company_id.tbai_protected_data_txt
+            ):
+                description_line = self.company_id.tbai_protected_data_txt[:250]
+            price_unit = line.tbai_get_price_unit()
+            lines.append(
+                (
+                    0,
+                    0,
+                    {
+                        "description": description_line,
+                        "quantity": line.tbai_get_value_cantidad(),
+                        "price_unit": "%.8f" % price_unit,
+                        "discount_amount": line.tbai_get_value_descuento(price_unit),
+                        "amount_total": line.tbai_get_value_importe_total(),
+                    },
+                )
+            )
+        return lines
+
+    def tbai_prepare_invoice_tax_values(self):
+        self.ensure_one()
+        partner = self.partner_id.commercial_partner_id
+        taxes_list = []
+        taxes = {}
         for line in self.lines:
             for tax in line.tax_ids_after_fiscal_position:
                 taxes.setdefault(
@@ -130,7 +168,7 @@ class PosOrder(models.Model):
                     {
                         "is_subject_to": True,
                         "is_exempted": False,
-                        "not_exempted_type": NotExemptedType.S1.value,
+                        "not_exempted_type": "S1",
                         "base": 0.0,
                         "amount": tax.amount,
                         "amount_total": 0.0,
@@ -149,108 +187,26 @@ class PosOrder(models.Model):
                 )
                 taxes[tax.id]["base"] += computed_tax["total_excluded"]
                 taxes[tax.id]["amount_total"] += amount_total
-            if is_gipuzkoa_tax_agency or is_araba_tax_agency:
-                lines.append(
-                    Command.create(
-                        {
-                            "description": line.name,
-                            "quantity": f"{line.qty:.2f}",  # noqa: E231
-                            "price_unit": f"{line.price_unit:.8f}",  # noqa: E231
-                            "discount_amount": "%.2f"
-                            % (line.qty * line.price_unit * line.discount / 100.0),
-                            "amount_total": f"{line.price_subtotal_incl:.2f}",  # noqa: E231
-                        }
-                    )
-                )
-        vals["tbai_tax_ids"] = []
-        for tax_values in taxes.values():
-            tax_values["base"] = "{:.2f}".format(tax_values["base"])
-            tax_values["amount"] = "{:.2f}".format(tax_values["amount"])
-            tax_values["amount_total"] = "{:.2f}".format(tax_values["amount_total"])
-            vals["tbai_tax_ids"].append(Command.create(tax_values))
-        if is_gipuzkoa_tax_agency or is_araba_tax_agency:
-            vals["tbai_invoice_line_ids"] = lines
-        return vals
+        for _tax_id, tax_values in taxes.items():
+            tax_values["base"] = "%.2f" % tax_values["base"]
+            tax_values["amount"] = "%.2f" % tax_values["amount"]
+            tax_values["amount_total"] = "%.2f" % tax_values["amount_total"]
+            taxes_list.append((0, 0, tax_values))
+        return taxes_list
 
-    def _tbai_build_invoice(self):
-        TBAIInvoice = self.env["tbai.invoice"]
-        for record in self:
-            vals = record.tbai_prepare_invoice_values()
-            tbai_invoice = TBAIInvoice.create(vals)
-            tbai_invoice.build_tbai_simplified_invoice()
-            record.tbai_invoice_id = tbai_invoice.id
-
-    @api.model
-    def _process_order(self, pos_order, draft, existing_order):
-        if draft:
-            return super()._process_order(pos_order, draft, existing_order)
-
-        tbai_vat_regime_key = pos_order["data"].get("tbai_vat_regime_key")
-        if tbai_vat_regime_key and isinstance(tbai_vat_regime_key, str):
-            pos_order["data"]["tbai_vat_regime_key"] = (
-                self.env["tbai.vat.regime.key"]
-                .search([("code", "=", tbai_vat_regime_key)], limit=1)
-                .id
-            )
-
-        order_id = super()._process_order(pos_order, draft, existing_order)
-        order = self.browse(order_id)
-
-        if order.config_id.tbai_enabled and not pos_order.get("to_invoice", False):
-            vals = order.tbai_prepare_invoice_values(pos_order)
-            tbai_invoice = self.env["tbai.invoice"].sudo().create(vals)
-            order.tbai_invoice_id = tbai_invoice
-            order.config_id.tbai_last_invoice_id = tbai_invoice
-
-        return order.id
-
-    def _export_for_ui(self, order):
-        res = super()._export_for_ui(order)
-        tbai_invoice_id = order.tbai_invoice_id
-        if tbai_invoice_id:
-            res["tbai_identifier"] = tbai_invoice_id.tbai_identifier
-            res["tbai_qr_src"] = "data:image/png;base64," + str(
-                order.tbai_invoice_id.qr.decode("UTF-8")
-            )
-        return res
-
-    def _prepare_invoice_vals(self):
-        res = super()._prepare_invoice_vals()
-        if self.tbai_enabled:
-            vat_regime_key_id = False
-            if self.tbai_vat_regime_key:
-                vat_regime_key_id = self.tbai_vat_regime_key.id
-            elif self.fiscal_position_id:
-                vat_regime_key_id = self.fiscal_position_id.tbai_vat_regime_key.id
-            elif self.partner_id:
-                fp = self.env["account.fiscal.position"]._get_fiscal_position(
-                    self.partner_id
-                )
-                vat_regime_key_id = fp.tbai_vat_regime_key.id
-            if not vat_regime_key_id:
-                vat_regime_key_id = self.env.ref(
-                    "l10n_es_ticketbai.tbai_vat_regime_01"
-                ).id
-            res.update({"tbai_vat_regime_key": vat_regime_key_id})
-            if self.tbai_invoice_id:
-                res.update(
-                    {
-                        "tbai_substitute_simplified_invoice": True,
-                        "tbai_substitution_pos_order_id": self.id,
-                    }
-                )
-        return res
+    def tbai_is_invoice_refund(self):
+        return False
 
     def tbai_get_value_serie_factura(self):
         if hasattr(self.config_id, "pos_sequence_by_device") and self.pos_device_id:
             sequence = self.pos_device_id.sequence
         else:
             sequence = self.config_id.l10n_es_simplified_invoice_sequence_id
-        order_date = fields.Datetime.context_timestamp(
+        date = fields.Datetime.context_timestamp(
             self, fields.Datetime.from_string(self.date_order)
         ).strftime(DEFAULT_SERVER_DATE_FORMAT)
         prefix, suffix = sequence.with_context(
-            ir_sequence_date=order_date, ir_sequence_date_range=order_date
+            ir_sequence_date=date, ir_sequence_date_range=date
         )._get_prefix_suffix()
         return prefix
 
@@ -265,14 +221,49 @@ class PosOrder(models.Model):
             )
         return self.l10n_es_unique_id[len(invoice_number_prefix) :]
 
-    def tbai_get_value_fecha_expedicion_factura(self):
+    def tbai_get_value_fecha_exp_factura(self):
         date = fields.Datetime.context_timestamp(
             self, fields.Datetime.from_string(self.date_order)
         )
         return date.strftime("%d-%m-%Y")
 
-    def tbai_get_value_hora_expedicion_factura(self):
+    def tbai_get_value_hora_exp_factura(self):
         date = fields.Datetime.context_timestamp(
             self, fields.Datetime.from_string(self.date_order)
         )
         return date.strftime("%H:%M:%S")
+
+    def tbai_get_value_simplified_invoice(self):
+        return "S"
+
+    def tbai_get_value_base_rectificada(self):
+        return 0.0
+
+    def tbai_get_value_cuota_rectificada(self):
+        return 0.0
+
+    def tbai_get_value_fecha_operacion(self):
+        return self.tbai_get_value_fecha_exp_factura()
+
+    def tbai_get_value_retencion_soportada(self):
+        return None
+
+    def _compute_tbai_description(self):
+        self.tbai_description_operation = "/"
+
+
+class PosOrderLine(models.Model):
+    _name = "pos.order.line"
+    _inherit = ["pos.order.line", "tbai.mixin.line"]
+
+    def tbai_get_value_cantidad(self):
+        return "%.2f" % self.qty
+
+    def tbai_get_value_descuento(self, price_unit):
+        return "%.2f" % (self.qty * price_unit * self.discount / 100.0)
+
+    def tbai_get_value_importe_total(self):
+        return "%.2f" % self.price_subtotal_incl
+
+    def tbai_get_price_unit(self):
+        return "%.8f" % self.price_unit
